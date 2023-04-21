8353233af86f2d2317ce9d896f11088def0fe7e1
Meaticus <117058929+Meaticus22@users.noreply.github.com>
2023-04-21 00:15:03 -0400

    Create mackali.py
    main file when running

---
+++ mackali.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def main():
+    # define categories and tools here
+    
+    # main logic for the tool goes here
+
+if __name__ == '__main__':
+    main()
+    
